7da8fde34f902caf1a10ee387088ed7a9b29d9de
mohamed morshed <mohamed_mohamed983@yahoo.com>
2023-08-03 07:39:40 +0000

    first

---
+++ MEAN-CRUD/server/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const server = '127.0.0.1:27017' // REPLACE WITH YOUR OWN SERVER
+const database = 'mean-crud'          // REPLACE WITH YOUR OWN DB NAME
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb://${server}/${database}`)
+
+    console.log('MongoDB connected!!')
+  } catch (err) {
+    console.log('Failed to connect to MongoDB', err)
+  }
+}
+
+connectDB()
